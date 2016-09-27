d1a6edd03a88601a95779e2e1fc0bcf7583d50ee
khritsanaphong fuangthong <kimochiii_555@hotmail.com>
2016-09-27 20:03:48 +0700

    test tennis02

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,5 +1,8 @@
 describe("A suite", function() {
   it("contains spec with an expectation", function() {
     expect(true).toBe(true);
+
+    // aaaaaaaaaaassssssssssddddddddddddddddddddddddddddddddfdfsaffs
+
   });
 });
