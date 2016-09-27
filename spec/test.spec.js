0bb8ddb562753a83718750be51bb6952138038a0
57160657 <wantteen.1st@gmail.com>
2016-09-27 20:12:15 +0700

    edit

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,8 +1,16 @@
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
+function Tennis(){
+  this.start = function() {
 
-    // aaaaaaaaaaassssssssssddddddddddddddddddddddddddddddddfdfsaffs
+  };
+  this.echo = function() {
+    return 'Love - Love';
+  }
+}
 
-  });
-});
+describe('Tennis game', function(){
+  it('should echo "Love - Love" when starting the game', function(){
+    var tennis = new Tennis();
+    tennis.start();
+    expect(tennis.echo()).toEqual('Love - Love');
+  })
+})
