6c01cb666048d861b43e76ffca0bea30494c22dd
khritsanaphong fuangthong <kimochiii_555@hotmail.com>
2016-09-28 21:05:14 +0700

    tennis A01

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,16 +1,28 @@
 function Tennis(){
+  var scoreA = 0;
+  var scoreB = 0;
   this.start = function() {
-
+    this.scoreA = function() {
+      if(scoreA == 0){
+        scoreA = 15;
+      }
+    }
   };
   this.echo = function() {
-    return 'Love - Love';
+    if(scoreA === 0 && scoreB === 0){
+      return 'Love - Love';
+    }
+    if(scoreA == 15 && scoreB == 0){
+      return 'Fifteen - Love';
+    }
   }
 }
 
-describe('Tennis game', function(){
-  it('should echo "Love - Love" when starting the game', function(){
-    var tennis = new Tennis();
+  var tennis = new Tennis();
+
+describe('The Game', function(){
+  it('The Gameeeeeee "Love - Love" Start !!!! ', function(){
     tennis.start();
     expect(tennis.echo()).toEqual('Love - Love');
   })
-})
+});
